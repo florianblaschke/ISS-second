5189215f22ba54accea33cc7be61416873512da3
Florian Blaschke <florianblaschke@web.de>
2023-06-21 21:18:04 +0200

    it works smh

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,44 +1,28 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
+  const {
+    data: coords,
+    error,
+    mutate,
+  } = useSWR(URL, {
+    refreshInterval: 5000,
   });
-
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
+  console.log("what are you?", coords);
+  if (error) return <div>Shit happened</div>;
+  if (coords === undefined) return <div>Still loading</div>;
   return (
     <main>
       <Map longitude={coords.longitude} latitude={coords.latitude} />
       <Controls
         longitude={coords.longitude}
         latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        onRefresh={() => mutate()}
       />
     </main>
   );
